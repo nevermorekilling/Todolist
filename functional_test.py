15e68308c68f1e6f0b9e92ec18d74a88054feba3
ChenAng <nevermorekillingspree@gmail.com>
2021-07-14 22:15:19 +0900

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,7 +14,32 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retireve_it_later(self):
         self.browser.get('http://localhost:8000')
 
+        # she notices the page title and header mention to-do lists
+        # assert 'To-Do' in browser.title, "Browser title was " + browser.title
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute(
+            'placeholder'), 'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby is tying
+        # fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates , and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.Enter)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers')
+                        for row in rows)
+        # There is still a text box inviting her to add another item.
+        # She enter "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         self.fail('Finish the test!')
 
 
@@ -25,20 +52,6 @@ if __name__ == '__main__':
 # browser = webdriver.firefox()
 #  browser.get('http://localhost:8000')
 
-# she notices the page title and header mention to-do lists
-# assert 'To-Do' in browser.title, "Browser title was " + browser.title
-
-# She is invited to enter a to-do item straight away
-
-# She types "Buy peacock feathers" into a text box (Edith's hobby is tying
-# fly-fishing lures)
-
-# When she hits enter, the page updates , and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a text box inviting her to add another item.
-# She enter "Use peacock feathers to make a fly" (Edith is very
-# methodical)
 
 # The page updates again,and now shows both items on her list
 
